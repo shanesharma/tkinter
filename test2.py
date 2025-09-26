eba97d7738ac95c6796f97abc62e09ab811523f2
shanesharma <shanessharma@gmail.com>
2025-09-26 08:45:28 -0700

    Added dynamic sizing for boxes

--- test2.py
+++ test2.py
@@ -5,17 +5,32 @@ root = tk.Tk()
 root.title("Linear search")
 root.geometry("800x600")
 
+win_width = 800
+win_height = 600
+box_height = 60
+'''
+TODO
+    1. add more buttons
+    2. divide buttons into files
+    3. dynamic cast buttons instead of manual cast
+
+
+'''
+
+
+default_font = ("Arial", 34)
+
 def iterate_array(index = 0):
         if index < 5:
-            canv.itemconfig(rect_arr[index], fill="yellow", font=("Arial", 34, "bold"))
+            canv.itemconfig(rect_arr[index], fill="yellow", font=default_font)
             root.after(500, iterate_array, index+1)
 
 
 def linear_search(val, index = 0):
-    if index == 5:
-        canv.create_text(400, 400, text="Entry not found", font=("Arial", 34 ))
+    if index == len(arr):
+        canv.create_text(400, 400, text="Entry not found", font=default_font)
     elif arr[index] == val:
-        canv.create_text(400, 400, text="Entry found", font=("Arial", 34 ))
+        canv.create_text(400, 400, text="Entry found", font=default_font)
         canv.itemconfig(rect_arr[index], fill="green")
     else:
         canv.itemconfig(rect_arr[index], fill="yellow")
@@ -32,34 +47,30 @@ def create_lin_window():
      canv =  tk.Canvas(linear_search_frame, width=800, height = 600, bg = "white")
      canv.grid(row=0, column=0)
      
-     arr = [1,4,5,9,3]
+     arr = [1,4,5,9,3,7,21,35]
 
      rect_arr = list()
+     size_list = len(arr)
+     gap = (win_width - size_list*80)/(size_list+1)
      for i,v in enumerate(arr):
           
 
-        rect_arr.append(canv.create_rectangle((i)*100 + 20, 200, (i)*100 + 100,300, fill= "lightblue", outline = "black"))
-        canv.create_text(((i)*100 + 60), 250 , text = v)
+        rect_arr.append(canv.create_rectangle(gap + i * (80 + gap), (win_height-box_height)/2, gap + i * (80 + gap) + 80,(win_height+box_height)/2, fill= "lightblue", outline = "black"))
+        canv.create_text((gap + i * (80 + gap) + 40), (win_height/2) , text = v)
         canv.grid(row=0, column=0)
     
      linear_search(11)
         
 
+if __name__ == "__main__":
+    main_frame = tk.Frame(root)
+    main_frame.grid(row=0, column=0, sticky= (tk.N, tk.W, tk.E, tk.S))
 
-main_frame = tk.Frame(root)
-main_frame.grid(row=0, column=0, sticky= (tk.N, tk.W, tk.E, tk.S))
-
-linear_search_button = tk.Button(main_frame, text = "Linear Search", command= create_lin_window)
-linear_search_button.grid(row=0, column=0)
-
-
+    linear_search_button = tk.Button(main_frame, text = "Linear Search", command= create_lin_window)
+    linear_search_button.grid(row=0, column=0)
 
 
-
-
-
-
-root.mainloop()
+    root.mainloop()
 
 
 
